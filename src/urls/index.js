9cfe9e46b701cbe5429b23b49a850f2e32c3391d
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-03-01 13:56:54 +0900

    [add] install styled-components, Material-UI

--- src/urls/index.js
+++ src/urls/index.js
@@ -6,5 +6,3 @@ export const foodsIndex = (restaurantId) =>
 export const lineFoods = `${DEFAULT_API_LOCALHOST}/line_foods`;
 export const lineFoodsReplace = `${DEFAULT_API_LOCALHOST}/line_foods/replace`;
 export const orders = `${DEFAULT_API_LOCALHOST}/orders`
-
-
