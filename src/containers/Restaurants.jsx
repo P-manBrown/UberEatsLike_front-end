db47149aed3e1a74f93ec973a30907c56df32103
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-03-01 15:43:45 +0900

    [add] 5-2

--- src/containers/Restaurants.jsx
+++ src/containers/Restaurants.jsx
@@ -1,5 +1,33 @@
-import React, { useEffect } from "react"
-import { fetchRestaurants } from "../apis/restaurants"
+import React, { useEffect } from "react";
+// styled-componentsを利用可能にする
+import styled from "styled-components";
+
+// apis
+import { fetchRestaurants } from "../apis/restaurants";
+
+// images
+import MainLogo from "../images/logo.png";
+import MainCoverImage from "../images/main-cover-image.png";
+
+// divに対するスタイル
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`
+
+// imgに対してスタイルを適用
+const MainCover = styled.img`
+  height: 600px;
+`
 
 export const Restaurants = () => {
   useEffect(() => {
@@ -10,7 +38,15 @@ export const Restaurants = () => {
   }, [])
 
   return (
-      <div>レストラン一覧</div>
+    <>
+      <HeaderWrapper>
+        {/* MainLogoImageの実態はimgタグなのでその後にsrcやaltを記述する */}
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </>
   )
 }
 
