76e48c649b3ea9b31c2447dc00ccafc134895f66
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-03-03 10:43:52 +0900

    [add] 6-2

--- src/containers/Foods.jsx
+++ src/containers/Foods.jsx
@@ -1,24 +1,47 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+// reducers
+import { initialState as foodsInitialState, foodsActionTypes, foodsReducer } from '../reducers/foods';
 // apis
-import { fetchFoods } from '../apis/foods'
+import { fetchFoods } from '../apis/foods';
+// constants
+import { REQUEST_STATE } from '../constants';
+
 
 
 export const Foods = () => {
   // useParamsでパラメーターを受け取ることができる
   const { restaurantsId } = useParams()
 
+  const [ foodsState, dispatch] = useReducer( foodsReducer, foodsInitialState);
+
   useEffect(() => {
+    dispatch({ type: foodsActionTypes.FETCHING });
     fetchFoods(restaurantsId)
-    .then((data) =>
-      console.log(data)
-    )
+      .then((data) => {
+        dispatch({
+          type: foodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
   }, [])
 
   return (
     <>
-      <div>フード一覧</div>
-      <p>restaurantsIdは{restaurantsId}です。</p>
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <>
+            <p>ロード中...</p>
+          </>
+        :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
     </>
   )
 }
