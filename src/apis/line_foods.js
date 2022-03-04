4aa8990b0b71c98f8b9f1ae68e57954e62b7fd5b
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-03-04 15:10:50 +0900

    [add] 7-1

--- src/apis/line_foods.js
+++ src/apis/line_foods.js
@@ -37,3 +37,34 @@ export const replaceLineFoods = (params) => {
   })
   .catch((e) => { throw e; })
 };
+
+export const fetchLineFoods = () => {
+  return axios.get(lineFoods)
+  // throw hoge;で例外を生成する。あえてエラーを定義して処理を停止させるために使用する。
+  // var hoge = 1;
+  // throw hoge;
+  // console.log(hoge);
+  // × VM112:2 Uncaught 1（console.log(hoge);は実行されない）
+  // throw new Error("エラーメッセージを引数に渡すことができる")
+  // 単なる文字列ではなく、APIから返ってきたErrorオブジェクトの中身に入れることができる。
+  // 単にエラー文を表示させるだけであればconsole.error("エラー"))でも可能。
+  // そのため、throwはtry...catch...やthen...catch...の中で使用することに意味がある。
+  // try {
+  //   console.log("正常な処理開始...")
+  //   throw new Error("なにかエラーが起きました！");  // ここで例外発生させると
+  //   console.log("処理が完了しました");              // ここは表示されない
+  // } catch(e) {                                      // eという変数名でErrorオブジェクトを捕まえ
+  //   console.log("エラーの内容は...");
+  //   console.error(e);                               // ここで出力される
+  // }
+  // throwでエラーを投げ、catchで捕まえる（catchに入る）。
+  .then(res => {
+    return res.data
+  })
+  // 例外が発生した際にはcatchを実行する。
+  // catchの中身はエラーをthrowする。
+  // throwすることで他の箇所でエラー文を取得することができる。
+  .catch((e) => { throw e; })
+};
+
+
--- src/containers/Orders.jsx
+++ src/containers/Orders.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import { fetchLineFoods } from '../apis/line_foods'
+import { initialState, lineFoodsActionTypes, lineFoodsReducer } from '../reducers/lineFood';
+
 
 export const Orders = () => {
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING })
+    fetchLineFoods()
+      .then((data) =>
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
+      )
+      // line_foods.jsのfetchLineFoodsでthrowしたエラーをcatch
+      .catch((e) => {
+        console.error(e)
+      });
+  }, []);
+
+
   return (
     <div>注文画面</div>
   )
 }
+
